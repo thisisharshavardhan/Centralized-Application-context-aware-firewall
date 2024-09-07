479e07a0e4758dcc3c1ad1e5ac781f022a391364
mohammadsharu <145218971+mohammadsharu@users.noreply.github.com>
2024-09-07 15:51:31 +0530

    Update package dependencies and add new endpoint to retrieve installed applications

--- Frontend/Centarlized Firewall Web Console/src/App.jsx
+++ Frontend/Centarlized Firewall Web Console/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Firewall from './Firewall/Firewall'
 
 function App() {
 
   return (
     <>
-      
+      <Firewall/>
     </>
   )
 }
--- Frontend/Centarlized Firewall Web Console/src/Firewall/Firewall.jsx
+++ Frontend/Centarlized Firewall Web Console/src/Firewall/Firewall.jsx
@@ -1,6 +1,33 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import axios from 'axios'
 function Firewall() {
+    const [Programs, setPrograms] = useState([])
+    useEffect(() => {
+         axios.get('http://localhost:5000/api/web-console/get-programs-list')
+        .then((response) => {
+            setPrograms(response.data)
+            console.log(Programs)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }, [])
   return (
-    <div>Firewall</div>
+    <>
+        <h4>Centralized application-context aware firewall</h4>
+        <h5><span>List of Programs in Device: </span></h5>
+           {
+                Programs.map((program, index) => {
+                    if(!program.DisplayName) {
+                        return null
+                    }
+                    else{
+                        return <p key={index}> <span>{index}  </span>  {program.DisplayName}</p>
+                    }
+                })
+           }
+    </>
   )
 }
 
